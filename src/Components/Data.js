2c3cf6eb17f7fee9a4f6ac17636937a64fdb8173
kn3929008@gmail.com <kn3929008@gmail.com>
2023-08-16 14:51:00 +0530

    Testimonials using react

---
+++ src/Components/Data.js
@@ -0,0 +1,40 @@
+const reviews=[
+    {
+        id:1,
+        name:"Pranay Gupta",
+        job:"Software Engineer",
+        image:"https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text:"I have had the pleasure of working with  this team on several projects,and i am consistent in my work"
+    },
+    {
+        id:2,
+        name:"Abir Pal",
+        job:"Graphic Designer",
+        image:"https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text:"I have been working with this company for several years now, and i have always been interested working here "
+    },
+    {
+        id:3,
+        name:"Soumya Banerjee",
+        job:"Marketing Manager",
+        image:"https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text:"I am thrilled with the result of our current marketing campaign, and it would have been pleasure for me working in sales "
+    },
+    {
+        id:4,
+        name:"Saikat Mukherjee",
+        job:"Content Stratergist",
+        image:"https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text:"I have been working with this company as a content creater for several years now,but none has impressed me"
+    },
+    {
+        id:5,
+        name:"Aritra Biswas",
+        job:"Data Analyst",
+        image:"https://avatars.githubusercontent.com/u/93366359?v=4",
+        text:"I have been working with this company as a Data analyst for several years now,and had a great experience working in this team"
+    },
+    
+]
+
+export default reviews
